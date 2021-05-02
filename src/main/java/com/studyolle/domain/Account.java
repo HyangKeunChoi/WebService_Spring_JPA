ef6150ef75d255d8d35e7c48bd85f5892c895f9b
hyangkeunchoi <gidrms99@naver.com>
2021-05-02 10:34:12 +0900

    add 회원 가입 컨트롤러

--- src/main/java/com/studyolle/domain/Account.java
+++ src/main/java/com/studyolle/domain/Account.java
@@ -1,12 +1,54 @@
 package com.studyolle.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
+@Getter @Setter @EqualsAndHashCode(of = "id") // 연관관계 순환 참조문제가 발생하여 id만 설정
+@Builder @AllArgsConstructor @NoArgsConstructor
 public class Account {
 
     @Id @GeneratedValue
     private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    // 가입날짜
+    private LocalDateTime joinedAt;
+
+    // 자기소개
+    private String bio;
+
+    private String url;
+
+    // 직업
+    private String occupation;
+
+    // 거주지
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByWeb;
 }
