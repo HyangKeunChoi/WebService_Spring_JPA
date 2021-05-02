8c3ebab698002b4502908bcbdb18e426cc2c1c2c
hyangkeunchoi <gidrms99@naver.com>
2021-05-02 12:16:16 +0900

    add 회원 가입: 폼 서브밋 검증

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -1,19 +1,44 @@
 package com.studyolle.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
 
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
     @GetMapping("/sign-up")
-    public String singUpForm(Model model) {
+    public String signUpForm(Model model) {
         // model.addAttribute("signUpForm", new SignUpForm());
         // 생략시 , 객체의 camelCase대로 attribute를 전달한다
         model.addAttribute( new SignUpForm());
         return "account/sign-up";
     }
 
+    // 폼서브밋
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if(errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        // TODO 회원 가입 처리
+        return "redirect:/";
+    }
 
 }
