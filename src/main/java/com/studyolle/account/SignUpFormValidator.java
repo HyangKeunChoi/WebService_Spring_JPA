80caa403e5e4b1ee35a8a10ba9d93660de9a1f84
hyangkeunchoi <gidrms99@naver.com>
2021-05-06 22:04:14 +0900

    add 회원 가입: 폼 서브밋 처리

--- src/main/java/com/studyolle/account/SignUpFormValidator.java
+++ src/main/java/com/studyolle/account/SignUpFormValidator.java
@@ -17,14 +17,14 @@ public class SignUpFormValidator implements Validator {
     }
 
     @Override
-    public void validate(Object o, Errors errors) {
+    public void validate(Object object, Errors errors) {
         // TODO email, nickname 중복 검사
-        SignUpForm signUpForm = (SignUpForm) errors;
+        SignUpForm signUpForm = (SignUpForm) object;
         if(accountRepository.existsByEmail(signUpForm.getEmail())) {
             errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "이미 사용중인 이메일 입니다.");
         }
 
-        if(accountRepository.existsByNickName(signUpForm.getNickname())) {
+        if(accountRepository.existsByNickname(signUpForm.getNickname())) {
             errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "이미 사용중인 닉네임 입니다.");
         }
     }
--- src/main/java/com/studyolle/domain/Account.java
+++ src/main/java/com/studyolle/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id") // 연관관계 순환 참조문제가 발생하여 id만 설정
@@ -51,4 +52,8 @@ public class Account {
     private boolean studyEnrollmentResultByWeb;
 
     private boolean studyUpdatedByWeb;
+
+    public void generateEmailCheckToken() {
+        this.emailCheckToken = UUID.randomUUID().toString();
+    }
 }
